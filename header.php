99fdc532b290fb5c2e975f5d8aaf7ea56545d10e
nicoleengberg <engberg.nicole@gmail.com>
2022-03-18 11:50:44 +0100

    trying php
    want to move some code out, to call on in several files

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html">
+<html lang="en">
+
+<head>
+  <title>Nicole Engberg</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<hr>
+<div id="container" , class="header">
+
+  <div>
+    <h1 title="so cool"><a href="index.html">Nicole Engberg</a></h1>
+  </div>
+
+  <div id="container" , class="links">
+    <a href="index.html">home</a>&nbsp;&nbsp;&nbsp;
+    <a href="aboutMe.html">about me</a>&nbsp;&nbsp;&nbsp;
+    <a href="projects.html">projects</a>&nbsp;&nbsp;&nbsp;
+  </div>
+
+</div>
+<hr>
