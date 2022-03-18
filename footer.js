193382b6158cfce3dd1e759c78432305a288c17f
nicoleengberg <engberg.nicole@gmail.com>
2022-03-18 22:22:11 +0100

    added js
    for header and footer, still need footer to stick to bottom

---
+++ footer.js
@@ -0,0 +1,6 @@
+
+document.write("<div class=\"footer\">");
+document.write("  <div>");
+document.write("   ");
+document.write("  <\/div>");
+document.write("<\/div>");
---
+++ header.js
@@ -0,0 +1,16 @@
+
+document.write("<hr>");
+document.write("<div class=\"header\">");
+document.write("");
+document.write("  <div>");
+document.write("    <h1 title=\"so cool\"><a href=\"index.html\">Nicole Engberg<\/a><\/h1>");
+document.write("  <\/div>");
+document.write("");
+document.write("  <nav id=\"container\" , class=\"links\">");
+document.write("    <a href=\"index.html\">home<\/a>&nbsp;&nbsp;&nbsp;");
+document.write("    <a href=\"aboutMe.html\">about me<\/a>&nbsp;&nbsp;&nbsp;");
+document.write("    <a href=\"projects.html\">projects<\/a>&nbsp;&nbsp;&nbsp;");
+document.write("<\/nav>");
+document.write("");
+document.write("<\/div>");
+document.write("<hr>");
