8482dd738c161c4bd5021fc0faeebfe936167608
nicoleengberg <engberg.nicole@gmail.com>
2022-03-19 18:49:51 +0100

    some more to footer

--- footer.js
+++ footer.js
@@ -1,6 +1,20 @@
 
-document.write("<div class=\"footer\">");
-document.write("  <div>");
-document.write("   ");
+document.write("<div id=\"gridFooter\" , class=\"footer\">");
+document.write("  <div class=\"1\">");
+document.write(" <p> Hello <\/p>  ");
+document.write("  <\/div>");
+document.write("  <div class=\"2\">");
+document.write(" <p> this <\/p>  ");
+document.write("  <\/div>");
+document.write("  <div class=\"3\">");
+document.write("<h3>Get in Touch<\/h3>");
+document.write("<p> &#10146 Milan, Italy<\/p>");
+document.write("<p> &#9993 <a href=\"mailto:engberg.nicole@gmail.com\">engberg.nicole@gmail.com<\/a><\/p>");
+document.write("<p> &#9743 <a href=\"tel:+393517652272\">+39 351 765 2272<\/a><\/p>");
+document.write("<p> &#9712 <a href=\"https://www.linkedin.com/in/nicole-engberg/\">LinkedIn<\/a><\/p>");
+document.write("<\/div>");
+document.write("  <div class=\"item4\">");
+document.write("<hr>")
+document.write(" <p> Created and Designed by Nicole Engberg. <\/p>  ");
 document.write("  <\/div>");
 document.write("<\/div>");
